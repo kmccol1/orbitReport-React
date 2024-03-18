94bc12ee0a91eef9cbf5ef31ca3b43985f2d7d65
Kyle McColgan <kdmbus21@gmail.com>
2024-03-18 18:22:17 -0500

    Upload Assignment Six Task Four code.

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,14 @@
+import satData from "./satData.jsx";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
+
     <div>
+    {displaySats.map((sat, id) => {
+      return (<button onClick={() => filterByType(sat)} key={id}>{sat} Orbit</button>);
+})}
       <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
