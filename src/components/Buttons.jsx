60e3ff375de44ba6f8716d2a25d5a59b042454a4
Kyle McColgan <kdmbus21@gmail.com>
2024-03-18 19:12:29 -0500

    Upload Assignment Six Task Five and Six code.

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,13 +1,13 @@
 import satData from "./satData.jsx";
+import './styling.css';
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
 
-    <div>
+    <div className ="flex-container">
     {displaySats.map((sat, id) => {
       return (<button onClick={() => filterByType(sat)} key={id}>{sat} Orbit</button>);
 })}
-      <button>Placeholder Button</button>
       <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,27 @@
+import './styling.css';
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     {sat.map((data, id) => {
+       if (data.operational == true)
+       {
+         return (<tr key={id}> <td>{data.name}</td><td>{data.type}</td><td>{data.launchDate}</td><td>Active</td> </tr>);
+       }
+       else
+       {
+         return (<tr key={id}> <td>{data.name}</td><td>{data.type}</td><td>{data.launchDate}</td><td>Inactive</td> </tr>);
+      }
+
+})}
      </tbody>
    </table>
   );
