e7d6c411c0a01ed2747e8317ee18fd64bed149ec
Kyle McColgan <kdmbus21@gmail.com>
2024-03-18 19:21:58 -0500

    Fix the heading tag in Banner.jsx.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import './styling.css';
 
 function Banner()
 {
-    return (<><header>Orbit Report</header> <p>Click on the buttons to see the satellites in that orbit type</p></>);
+    return (<><h1>Orbit Report</h1> <p>Click on the buttons to see the satellites in that orbit type</p></>);
 }
 
 export default Banner;
